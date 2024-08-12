ccfed1d67b5f85a17b7c7740e497f3700617968f
(-_-)_Omar_Hossam_(-_-) <153639313+OmarHossam666@users.noreply.github.com>
2024-08-12 19:48:49 +0300

    Finishing all the challenges

--- 01-string-manipulation/projects/starter/bin/starter.dart
+++ 01-string-manipulation/projects/starter/bin/starter.dart
@@ -15,12 +15,12 @@ void main()
     print(rightTrimmed);
     print(allTrimmed);
 
-    final time = Duration(hours: 1 , minutes: 2 , seconds: 3);
-    final hours = time.inHours;
-    final minutes = '${time.inMinutes % 60}'.padLeft(2 , '0');
-    final seconds = '${time.inSeconds % 60}'.padLeft(2 , '0');
-    final timeString = '$hours:$minutes:$seconds';
-    print(timeString);
+    // final time = Duration(hours: 1 , minutes: 2 , seconds: 3);
+    // final hours = time.inHours;
+    // final minutes = '${time.inMinutes % 60}'.padLeft(2 , '0');
+    // final seconds = '${time.inSeconds % 60}'.padLeft(2 , '0');
+    // final timeString = '$hours:$minutes:$seconds';
+    // print(timeString);
 
     const csvFile = 'Martin,Emma,12,Paris,France';
     final fields = csvFile.split(',');
@@ -254,4 +254,89 @@ Mongolia''';
     {
         print('Your credit card is valid.');
     }
+
+    const htmlText = '''
+<!DOCTYPE html>
+<html>
+<body>
+<h1>Dart Tutorial</h1>
+<p>Dart is my favorite language.</p>
+</body>
+</html>''';
+
+    final start = htmlText.indexOf('<h1>') + '<h1>'.length; // The index of D (inclusive)
+    final end = htmlText.indexOf('</h1>'); // index of < (exclusive)
+    final heading = htmlText.substring(start , end);
+    print(heading); // Dart Tutorial
+
+    const anothertext = '''
+<h1>Dart Tutorial</h1>
+<h1>Flutter Tutorial</h1>
+<h1>Other Tutorials</h1>
+''';
+
+    // Using Substring
+    var position = 0; // An index to track down the searching area
+
+    while (true)
+    {
+        final start = anothertext.indexOf('<h1>' , position) + '<h1>'.length;
+        final end = anothertext.indexOf('</h1>' , position);
+
+        if (start == -1 || end == -1)
+        {
+            break;
+        }
+
+        final heading = anothertext.substring(start , end);
+        print(heading);
+
+        position = end + '</h1>'.length;
+    }
+
+    // Using Regex Groups
+    final headings = RegExp(r'<h1>(.+)</h1>');
+    final matchs = headings.allMatches(anothertext);
+    for (final match in matchs)
+    {
+        print(match.group(1));
+    }
+
+    // Challenge 01
+    final emailAddress = RegExp(r"^[a-zA-Z0-9.!#@$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$");
+    print(emailAddress.hasMatch('simple@example.com'));
+    print(emailAddress.hasMatch('very.common@example.com'));
+    print(emailAddress.hasMatch('disposable.style.email.with+symbol@example.com'));
+    print(emailAddress.hasMatch('user.name+tag+sorting@example.com'));
+    print(emailAddress.hasMatch('user.name@sub.example.com'));
+    print(emailAddress.hasMatch('user@domain@example.com'));
+    print(emailAddress.hasMatch('user@192.168.1.1'));
+
+    // Challenge 02
+    const song = '[00:12.34]Row, row, row your boat';
+
+    // Using Substring
+    final minutesStart = song.indexOf('[') + '['.length , minutesEnd = song.indexOf(':');
+    final secondsStart = song.indexOf(':') + ':'.length , secondsEnd = song.indexOf('.');
+    final hundredthsStart = song.indexOf('.') + '.'.length , hundredthsEnd = song.indexOf(']');
+    final lyricsStart = song.indexOf(']') + ']'.length;
+
+    print('Minutes: ${song.substring(minutesStart , minutesEnd)}');
+    print('Seconds: ${song.substring(secondsStart , secondsEnd)}');
+    print('Hundredths: ${song.substring(hundredthsStart , hundredthsEnd)}');
+    print('Lyrics: ${song.substring(lyricsStart)}');
+
+    // Using Regex Groups
+    final songRegex = RegExp(r'^\[(\d+):(\d+)\.(\d+)\](.+)$');
+    final songMatch = songRegex.firstMatch(song);
+
+    final minutes = songMatch?.group(1);
+    final seconds = songMatch?.group(2);
+    final hundredths = songMatch?.group(3);
+    final lyrics = songMatch?.group(4);
+
+    print('Minutes: $minutes');
+    print('Seconds: $seconds');
+    print('Hundredths: $hundredths');
+    print('Lyrics: $lyrics');
 }
